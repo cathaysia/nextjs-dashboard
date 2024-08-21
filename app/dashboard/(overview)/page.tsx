74f740166fb2d19b7e7f2ca7bd61943dc45f5c02
loongtao.zhang <loongtao.zhang@outlook.com>
2024-08-22 03:03:26 +0800

    fix(cards): fix cards skeleton
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,6 +6,7 @@ import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
 import {
 	CardSkeleton,
+	CardsSkeleton,
 	LatestInvoicesSkeleton,
 	RevenueChartSkeleton,
 } from '@/app/ui/skeletons';
@@ -17,7 +18,7 @@ export default async function Page() {
 				Dashboard
 			</h1>
 			<div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-				<Suspense fallback={<CardSkeleton />}>
+				<Suspense fallback={<CardsSkeleton />}>
 					<CardWrapper />
 				</Suspense>
 			</div>
